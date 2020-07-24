d28972b4a9c10b907652bb559ddee943f02dd67b
Iman Morovatian <mo.iman.1377@gmail.com>
2020-07-24 13:14:09 +0430

    add cors

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const CO = require('./middleWares/crossOrigin');
 const syncDB = require('./index');
 const question = require('./routers/question');
 const gateway = require('./middleWares/auth');
+const cors = require('cors');
 
 
 const app = express();
@@ -20,15 +21,31 @@ if(process.env.NODE_ENV == "development"){
 }
 
 app.use(express.json());
+app.use(gateway);
 app.use(CO);
+
+const corsOpts = {
+	origin: '*',
+  
+	methods: [
+	  'GET',
+	  'POST',
+	],
+  
+	allowedHeaders: [
+	  'Content-Type',
+	],
+  };
+app.use(cors(corsOpts))
+
 app.use('/auth/', auth);
 app.use('/user/', user);
-app.use(gateway);
 app.use('/question/', question);
 app.use('/favorite/', favorite);
+
 const log = debug('app::start');
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
 
 app.listen(port, async()=>{
 	log(`Server started on port ${port}`);
